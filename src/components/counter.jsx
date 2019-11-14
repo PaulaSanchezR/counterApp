66f721cd9a0a9903a1a67484719622ecf1925f83
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-11-14 14:34:12 -0500

    move method to parent component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Counter  extends Component {
 //    special properti that contein all the data that this compoment need
     state={
-        value:this.props.value,
+        //value:this.props.counter.value,
         imageUrl: "https://picsum.photos/200",
         tags: ["tag1","tag2","tag3"]
 
@@ -41,24 +41,23 @@ class Counter  extends Component {
                     //     // bind method 
                     // }
 
-//    to avoid this noise procedure we use arrow function inherante the this word
-    handleIncrement = (product) => {
-        console.log('Increment', product)
-        // this is an error we can not reasign any props value
-        // this.props.value = 0
-        //  only way to update the setState and we pass an object
-        this.setState({ count : this.state.value +1 })
-    }
+//    to avoid this noise procedure we use arrow function inherante the 'this" word
+    // handleIncrement = () => {
+    //     // this is an error we can not reasign any props value
+    //     // this.props.value = 0
+    //     //  only way to update the setState and we pass an object
+    //     this.setState({ value : this.state.value +1 })
+    // }
 
             // doHadleIncrement = () =>{
             //     this.handleIncrement( { id: 1})
             // }
     render() { 
-        console.log ('props', this.props)
+        //console.log ('props', this.props)
        return(
         <div>
             {/*  when we recive the children {this.props.children} */}
-            <h4>{this.props.id}</h4>
+            <h4>{this.props.counter.id}</h4>
             <img src={this.state.imageUrl} alt=""/>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             {/* JSX handle the event by reference. we are not calling the method just passing a references to it */}
@@ -67,13 +66,19 @@ class Counter  extends Component {
                     // onClick we pass the function references we can NOT call this.handleIncrement(1)
                     // we need to pass a function references 
                     // this producuere is messy  better soulution is use a inline function
-                     // onClick={this.doHadleIncrement} 
-                onClick={ () =>  this.handleIncrement(1)}
+                //  onClick={this.doHadleIncrement} 
+                // onClick={ () =>  this.handleIncrement(1)}
+                // onClick={this.handleIncrement}
+                onClick = {() => this.props.onIncrement(this.props.counter)}
                 className="btn btn-secondary btn-sm">increment</button>
 
                 {/* in order to delete the counter we need to delete on the counters component 
                 THE COMPONENT THAT OWNS A PIECE OF THE STATE, SHOULB BE THE ONE MODIFYING IT*/}
-            <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>    
+
+                {/* reference to the event on the parent component also we change the props and we are sending the counter object */}
+            {/* <button onClick={()=> this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>     */}
+            <button onClick={()=> this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>    
+
             {this.renderTags()}
         </div>
         );
@@ -81,13 +86,13 @@ class Counter  extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { value } = this.state;
-        console.log(value)
+        const { value } = this.props.counter;
+        //console.log(value)
         return value === 0 ? "Zero" : value;
     }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,21 +12,63 @@ class Counters extends Component {
         ]
      }
 
-     handleDelete = () => {
-         console.log('Evenvt handler called')
+     handleIncrement = counter=> {
+        // create new counter array with the spray operator we are clonning the array
+        const counters = [...this.state.counters] ;
+        // this sitaxis modifed the real value and for React this is NO NOOOOOO
+
+                // counters[0].value++;
+                // console.log('counter', this.state.counters[0])
+        const index = counters.indexOf(counter)
+        counters[index] = {...counters}
+        counters[index].value++;
+        //console.log('counter', this.state.counters[index])
+        console.log('index', index)
+
+        this.setState({ counters })
+     }
+
+     handleReset = ()=>{
+        //  new array
+         const counters = this.state.counters.map(c => {
+             c.value = 0;
+             return c
+         })
+        //  return the array
+         this.setState = { counters}
+     }
+
+     handleDelete = (counterId) => {
+         //console.log('Evenvt handler called0',counterId)
+        //  we dont update the state directly
+        // filter method to get all the counters except for the counterID
+        // we create the new array
+         const counters = this.state.counters.filter(c => c.id !== counterId)
+        this.setState({counters: counters})
      }
     render() { 
         return ( 
         <div>
+            <button 
+            className="btn btn-primary btn-sm m-2"
+            onClick= {this.handleReset}>Reset</button>
            {this.state.counters.map(counter => 
             //    all this properties are part of the props key, value, id
             //    props are read only
            <Counter 
-                key ={counter.id} 
+                key ={counter.id} // this key is use internally by react
+                // we are passing the references to the function to the child component
+                // 
                 onDelete={this.handleDelete} 
-                value={counter.value} 
-                id={counter.id}>
-           {/* this is when we pass a children */}
+                onIncrement={this.handleIncrement}
+
+                    // innecesari repetition we are going to encapsulate related values, if we add more properties to the state object we will need to add it manually
+                    // it will be messy  and ugly for this we going to pass the counter object 
+                        // value={counter.value} 
+                        // id={counter.id}
+                counter={counter}
+              >
+           {/* ejemplo this is when we pass a children */}
               {/* <h4>title</h4> */}
            </Counter>  // selected={true}
            )}
